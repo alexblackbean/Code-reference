d7f795c31afe657f406d089d3204b58d93f7cc19
alexblackbean <alexblackbean@gmail.com>
2023-07-14 15:54:34 +0800

    Initial Commit

---
+++ python/except.py
@@ -0,0 +1,38 @@
+'''
+author: Kuo Yu Liao
+email: alexgreenbean6092@gmail.com
+code content: Command line argument parser fo python
+'''
+class MyClass():
+    def __init__(self)->None:
+        self.height = 185
+        self.weight = 77
+        
+def AssertExample():
+    '''
+    AssertionError
+    assert statement fails
+    ''' 
+    try:
+        one = 1
+        one2 = 2
+        assert one == one2
+        print("assert sucess")
+    except AssertionError:
+        print("one2 setting failed")
+
+def AttributeExample():
+    '''
+    AttributeError
+    attribute reference, assignment fails
+    '''
+    try:
+        obj = MyClass()
+        print(obj.gender)
+    except AttributeError:
+        print("there is no attribute \"gender\" in MyClass")
+if __name__ == '__main__':
+    '''Usage'''
+    AssertExample()
+    AttributeExample()
+
