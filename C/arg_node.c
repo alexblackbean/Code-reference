0124242f4b41041f3710fdeff8baee6762a15325
alexblackbean <alexblackbean@gmail.com>
2023-09-09 15:40:29 +0800

    modified check condition

--- C/arg_node.c
+++ C/arg_node.c
@@ -16,30 +16,62 @@ flt_num: float type variable
  return:arg_node pointer
 */
 
-struct arg_node* ARG_Init(int int_num, float flt_num, char c, char* str)
+struct arg_node* ARG_Init(int int_num, float flt_num, char c, char* str, bool int_val, bool flt_val, bool char_val, bool str_val)
 {
     struct arg_node* tmp;
     tmp = malloc(sizeof(*tmp));
-    tmp->integer_num = int_num;
-    tmp->float_num = flt_num;
-    tmp->character = c;
-    tmp->string = calloc(STR_MAX_LEN, sizeof(char));
-    strcpy(tmp->string, str);
+    tmp->int_val = int_val;
+    tmp->flt_val = flt_val;
+    tmp->char_val = char_val;
+    tmp->str_val = str_val;
+
+    if (int_val)
+    {
+        tmp->integer_num = int_num;
+    }
+    if (flt_val)
+    {
+        tmp->float_num = flt_num;
+    }
+    if (char_val)
+    {
+        tmp->character = c;
+    }
+    if (str_val)
+    {
+        tmp->string = calloc(STR_MAX_LEN, sizeof(char));
+        strcpy(tmp->string, str);
+    }
     return tmp;
 }
 /* Delete and release the argument node*/
 void ARG_delete(struct arg_node* tmp)
 {
-    free(tmp->string);
-    free(tmp);
+    if (tmp)
+    {
+        if (tmp->str_val)
+        {
+            free(tmp->string);
+        }
+        free(tmp);
+    }
 }
-void ARG_show(struct arg_node* tmp);
+void ARG_show(struct arg_node* tmp)
 {
-    printf("The argument node has integer: %d\n", tmp->integer_num);
-    printf("The argument node has float: %f\n", tmp->float_num);
-    printf("The argument node has character: %c\n", tmp->character);
-    if (tmp->string)
+    if (tmp->int_val)
+    {
+        printf("The argument node has integer: %d\n", tmp->integer_num);
+    }
+    if (tmp->flt_val)
+    {
+        printf("The argument node has float: %f\n", tmp->float_num);
+    }
+    if (tmp->char_val)
+    {
+        printf("The argument node has character: %c\n", tmp->character);
+    }
+    if (tmp->str_val)
+    {
         printf("The argument node has string: %s\n", tmp->string);
-    else
-        printf("The argument node doesn't have a string");
+    }
 }
--- C/arg_node.h
+++ C/arg_node.h
@@ -3,8 +3,8 @@ author: Kuo Yu Liao
 email: alexgreenbean6092@gmail.com
 code content: Argument Node interface (C)
 */
-#ifndef ARG_NODE
-#define ARG_NODE
+#ifndef ARG_NODE_H
+#define ARG_NODE_H
 #define STR_MAX_LEN 100
 #include <stdbool.h>
 typedef struct arg_node
@@ -13,13 +13,17 @@ typedef struct arg_node
     float float_num; // float
     char character; // char
     char* string; // string
+    bool int_val;
+    bool flt_val;
+    bool char_val;
+    bool str_val;
 } ARG_NODE;
 
 /*New a node containing various data*/
-struct arg_node* ARG_Init(int, float, char, char*);
+struct arg_node* ARG_Init(int, float, char, char*, bool, bool, bool, bool);
 /*release memory that node use*/
 void ARG_delete(struct arg_node*);
 /*show all datatype that argument node has*/
 void ARG_show(struct arg_node*);
 
-#ifndef
+#endif
